284caae18f2b7819111b8dae72f4542304e453d6
xcx0902 <3562907147@qq.com>
2023-01-09 12:34:00 +0800

    Update b.cpp

--- nflsoj/Contest1599/b.cpp
+++ nflsoj/Contest1599/b.cpp
@@ -9,13 +9,13 @@ int main() {
     for (int i = 1; i <= n; i++)
         for (int j = 1; j <= s[i].size(); j++) {
             bool f = 0;
-            string temp = s[i].substr(0, j);
+            string t = s[i].substr(0, j);
             for (int k = 1; k <= n; k++) {
                 if (k == i) continue;
-                if (temp == s[k].substr(0, j)) { f = 1; break; }
+                if (t == s[k].substr(0, j)) { f = 1; break; }
             }
-            if (!f || temp == s[i]) {
-                cout << s[i] << " " << temp << endl;
+            if (!f || t == s[i]) {
+                cout << s[i] << " " << t << endl;
                 break;
             }
         }
