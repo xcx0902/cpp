ffdba764cc503b0dd18e26c796030f27b4b3d2a5
xcx0902 <3562907147@qq.com>
2023-01-14 21:03:42 +0800

    Update deleteExe.bat

--- deleteExe.bat
+++ deleteExe.bat
@@ -1,8 +1,8 @@
 del /s /q *.exe
 del /s /q *.out
-:: Run this bat file will delete all .exe and .out 
-:: files in this directory (include its children
-:: directory, and children's children directory, and
-:: so on). You can put it in your C++ working root
-:: directory, then you can run it to delete all
-:: compiled files to minimize your disk usage.
+: Run this bat file will delete all .exe and .out 
+: files in this directory (include its children
+: directory, and children's children directory, and
+: so on). You can put it in your C++ working root
+: directory, then you can run it to delete all
+: compiled files to minimize your disk usage.
