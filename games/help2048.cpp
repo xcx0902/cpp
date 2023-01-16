2769c44070147588f78a1205205c183b11dc1541
xcx0902 <3562907147@qq.com>
2023-01-16 21:52:54 +0800

    Update help2048.cpp

--- games/help2048.cpp
+++ games/help2048.cpp
@@ -3,27 +3,27 @@
 using namespace std;
 
 void down(int vk) {
-	keybd_event(vk, 0, 0, 0);
+    keybd_event(vk, 0, 0, 0);
 }
 
 void up(int vk) {
-	keybd_event(vk, 0, KEYEVENTF_KEYUP, 0);
+    keybd_event(vk, 0, KEYEVENTF_KEYUP, 0);
 }
 
 void press(int vk) {
-	down(vk);
-	up(vk);
+    down(vk);
+    up(vk);
 }
 
 int main() {
-	Sleep(3000);
+    Sleep(3000);
     while (1) {
         int r = rand() % 15;
         if (r >= 0 && r <= 3) press(VK_UP), cout << "Press UP" << endl; 
         else if (r >= 4 && r <= 7) press(VK_DOWN), cout << "Press DOWN" << endl;
         else if (r >= 8 && r <= 11) press(VK_LEFT), cout << "Press LEFT" << endl;
         else if (r >= 11 && r <= 14) press(VK_RIGHT), cout << "Press RIGHT" << endl;
-		Sleep(30);
+        Sleep(30);
     }
-	return 0;
+    return 0;
 }
