72ad30ea20bd1ff6e7b70bf08786e1c8f1c448a1
xcx0902 <3562907147@qq.com>
2023-02-24 18:43:23 +0800

    Update e.cpp

--- atcoder/abc290/e.cpp
+++ atcoder/abc290/e.cpp
@@ -11,31 +11,31 @@ signed main() {
     lmid = (1 + n) >> 1;
     if (n % 2) rmid = lmid;
     else rmid = lmid + 1;
-    cerr << lmid << " " << rmid << endl;
+    // cerr << lmid << " " << rmid << endl;
     // step 1
-    cerr << "Step 1" << endl;
+    // cerr << "Step 1" << endl;
     for (int i = 1; i <= n; i++)
         cnt[a[i]]++, sum++;
     for (int i = 1; i <= lmid; i++) {
         cnt[a[i]]--, sum--;
         ans += i * (sum - cnt[a[i]]);
-        cerr << "i = " << i << endl;
-        cerr << "sum - cnt[a[i]] = " << sum - cnt[a[i]] << endl;
+        // cerr << "i = " << i << endl;
+        // cerr << "sum - cnt[a[i]] = " << sum - cnt[a[i]] << endl;
         cnt[a[n - i + 1]]--, sum--;
     }
     // step 2
-    cerr << "Step 2" << endl;
+    // cerr << "Step 2" << endl;
     sum = 0;
     memset(cnt, 0, sizeof cnt);
     for (int i = 1; i <= n; i++)
         cnt[a[i]]++, sum++;
-    // cerr << "sum = " << sum << endl;
+    // // cerr << "sum = " << sum << endl;
     for (int i = n; i >= rmid; i--) {
         cnt[a[i]]--, sum--;
         cnt[a[n - i + 1]]--, sum--;
         ans += (n - i + 1) * (sum - cnt[a[i]]);
-        cerr << "i = " << i << endl;
-        cerr << "sum - cnt[a[i]] = " << sum - cnt[a[i]] << endl;
+        // cerr << "i = " << i << endl;
+        // cerr << "sum - cnt[a[i]] = " << sum - cnt[a[i]] << endl;
     }
     // output
     cout << ans << endl;
